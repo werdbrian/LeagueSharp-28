c139523f200174a1311280b8b753ae407bd016d4
werdbrian <werdbrian@gmail.com>
2016-10-15 07:01:14 -0700

    Update Rumble.cs

--- xSaliceResurrected_Rework/Pluging/Rumble.cs
+++ xSaliceResurrected_Rework/Pluging/Rumble.cs
@@ -381,8 +381,25 @@
                 return;
 
             if (Menu.Item("Draw_Q", true).GetValue<bool>())
+                  if (menu.Item("Draw_Q", true).GetValue<bool>())
                 if (Q.Level > 0)
-                    Render.Circle.DrawCircle(Player.Position, Q.Range, Q.IsReady() ? Color.Green : Color.Red);
+                    {
+                        Render.Circle.DrawCircle(Player.Position, 600, Q.IsReady() ? Color.Green : Color.Red);
+                          var pos = ObjectManager.Player.Position.To2D() + 600 * ObjectManager.Player.Direction.To2D().Perpendicular();
+                    Drawing.DrawCircle(pos.To3D(), 50, Q.IsReady() ? Color.Green : Color.Red);
+                    var playerPosition = ObjectManager.Player.Position.To2D();
+                    var direction = ObjectManager.Player.Direction.To2D().Perpendicular();
+                    var currentScreenPlayer = Drawing.WorldToScreen(ObjectManager.Player.Position);
+                    const int distance = 600;
+                    var currentAngel = 30 * (float) Math.PI / 180;
+                    var currentCheckPoint1 = playerPosition + distance * direction.Rotated(currentAngel);
+                    currentAngel = 335 * (float) Math.PI / 180;
+                    var currentCheckPoint2 = playerPosition + distance * direction.Rotated(currentAngel);
+                    var currentScreenCheckPoint1 = Drawing.WorldToScreen(currentCheckPoint1.To3D());
+                    var currentScreenCheckPoint2 = Drawing.WorldToScreen(currentCheckPoint2.To3D());
+                    Drawing.DrawLine(currentScreenPlayer.X, currentScreenPlayer.Y, currentScreenCheckPoint1.X, currentScreenCheckPoint1.Y, 2,Q.IsReady() ? Color.Green : Color.Red);
+                    Drawing.DrawLine(currentScreenPlayer.X, currentScreenPlayer.Y, currentScreenCheckPoint2.X, currentScreenCheckPoint2.Y,2, Q.IsReady() ? Color.Green : Color.Red);
+                    }
 
             if (Menu.Item("Draw_W", true).GetValue<bool>())
                 if (W.Level > 0)
